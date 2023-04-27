20d7d1c313cdc2cc781a8cb9f1d362d4c7ddde8b
oraby <na3oraby@gmail.com>
2023-04-27 08:39:09 +0200

    create web site home page & link with models

--- personal/admin.py
+++ personal/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import About , Experience , Services , Projects , Reviews , Faq
+
 
 # Register your models here.
+admin.site.register(About)
+admin.site.register(Experience)
+admin.site.register(Services)
+admin.site.register(Projects)
+admin.site.register(Reviews)
+admin.site.register(Faq)
--- personal/views.py
+++ personal/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from .models import About , Experience , Services , Projects , Reviews , Faq
 
 # Create your views here.
 def home (request): 
-    return render(request,'home/home.html',{})
+    about = About.objects.first()
+    experience_first = Experience.objects.all()[0:2]
+    experience_last = Experience.objects.all()[2:]
+    services = Services.objects.all()
+    projects = Projects.objects.all()
+    reviews = Reviews.objects.all()
+    faq = Faq.objects.all()
+    return render(request,'home/home.html',{
+        'about':about ,
+        'experience_first':experience_first ,
+        'experience_last':experience_last ,
+        'services':services ,
+        'projects':projects , 
+        'reviews':reviews , 
+        'faq':faq
+    })
 
 
